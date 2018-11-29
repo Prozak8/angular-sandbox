7e1b12991c46088961b51e78833e810ff94bb3bc
Zak <zakary.howard94@gmail.com>
2018-11-29 16:10:38 +0100

    practicing on on click events and more databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -16,5 +18,11 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
   }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
