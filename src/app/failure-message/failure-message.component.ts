43715e87f6971f9c6d420092431074317ef165cb
Zak <zakary.howard94@gmail.com>
2018-11-29 11:26:10 +0100

    changed colours of paragraph text in failure and success components

--- src/app/failure-message/failure-message.component.ts
+++ src/app/failure-message/failure-message.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-failure-message]',
+  selector: 'app-failure-message',
   templateUrl: './failure-message.component.html',
   styleUrls: ['./failure-message.component.css']
 })
 export class FailureMessageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {   }
 
   ngOnInit() {
   }
