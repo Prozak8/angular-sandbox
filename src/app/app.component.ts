17ec528664949bb5d2e1768c12f2dcf5271996be
Zak <zakary.howard94@gmail.com>
2018-12-06 11:17:10 +0100

    practicing with ngOnChanges, ngOnInit, ngDoCheck, ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit, ngAfterViewChecked, ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,13 @@ export class AppComponent {
       name: blueprintData.serverName,
       content: blueprintData.serverContent
     });
-  }                    
+  } 
+  
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
