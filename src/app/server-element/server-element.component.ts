4d5204942daec2ebd5263e1ce8725fc88608dc16
Zak <zakary.howard94@gmail.com>
2018-12-06 11:35:30 +0100

    added content child referencing a local id to a text paragraph and console logging on init and after content loaded to see where it pops out in the console.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -12,7 +12,8 @@ import {
   AfterViewChecked, 
   OnDestroy, 
   ViewChild, 
-  ElementRef }  
+  ElementRef, 
+  ContentChild }  
   from '@angular/core';
 
 @Component({
@@ -35,6 +36,7 @@ OnDestroy {
                        content: string};
   @Input() name: string;
   @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { 
     console.log('constructor called!');
@@ -48,6 +50,7 @@ OnDestroy {
   ngOnInit() {
     console.log('ngOnInit called!');
     console.log(this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -56,6 +59,7 @@ OnDestroy {
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
